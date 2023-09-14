05afbbf69c8ba76296778ace9cd92865af6811b3
kevin beier <kevinb@codingtemple.com>
2023-09-14 17:22:37 -0400

    posts back to 5000

--- docker-compose-demo/app.py
+++ docker-compose-demo/app.py
@@ -32,4 +32,4 @@ def index():
     return f"This page has been visited {counter.hits} times!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
